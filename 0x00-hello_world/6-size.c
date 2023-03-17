d17ceaea6dcecfbd9cb5031e8b0b0b34dd482c54
MustafaAhmed313 <mostafa3132004@gmail.com>
2023-03-16 17:59:00 -0700

    fine

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with printf function
+ * Description: A C program that prints sizeof dataTypes using 
+ * sizeof  function
  *
  * Return: Always 0 (Success)
 */
@@ -11,7 +12,7 @@
 int main(void)
 {
 	printf("Size of a char: %ld byte(s)\n" , sizeof(char));
-	printf("Size of a int: %ld byte(s)\n" , sizeof(int));
+	printf("Size of an int: %ld byte(s)\n" , sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n" , sizeof(long int));
 	printf("Size of a long long int: %ld byte(s)\n" , sizeof(long long int));
 	printf("Size of a float: %ld byte(s)\n" , sizeof(float));
