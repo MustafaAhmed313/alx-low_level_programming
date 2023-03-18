708ef5651cfe52bc1d4df3cd64b44432e112aff7
MustafaAhmed313 <mostafa3132004@gmail.com>
2023-03-17 17:19:29 -0700

    fine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print ten digits consecutive representaion
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+		printf(n);
+	putchar('\n');
+	return (0);
+}
