1a63130d5e7d954a4af5370b33d6a45320e2c1a2
MustafaAhmed313 <mostafa3132004@gmail.com>
2023-03-17 17:30:03 -0700

    fine

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet consecutive representaion
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
