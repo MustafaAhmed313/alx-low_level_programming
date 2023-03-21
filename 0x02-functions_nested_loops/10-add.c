c9f145b84b261b8ea08e34016fe4fd0e620da7ad
MustafaAhmed313 <mostafa3132004@gmail.com>
2023-03-21 12:44:05 -0700

    fine

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - return addition of a and b.
+ *
+ * @a: operand 1
+ *
+ * @b: operand 2
+ *
+ * Return: always 0 (Success)
+ */
+int add(int a, int b);
+{
+	return (a + b);
+}
