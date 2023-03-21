bc685c3f391c1fc10a3146e442d79d418ec182f2
MustafaAhmed313 <mostafa3132004@gmail.com>
2023-03-21 11:39:18 -0700

    fine

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,7 +8,13 @@
  */
 int _isalpha(int c);
 {
-	if (c <= 'z' && c >= 'a')
+	if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
+	{
 		return (1);
-	return (0);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
 }
