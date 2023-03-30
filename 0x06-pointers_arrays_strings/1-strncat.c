1c3a15b4de3cd57ac35cea2424bc6fcb369676f2
MustafaAhmed313 <mostafa3132004@gmail.com>
2023-03-30 16:58:23 -0700

    fine

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,12 +12,13 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int c1, c2;
+	int c1, c2, c3;
 
 	c1 = 0;
 	while (dest[c1])
 		c1++;
-	for (c2 = 0 ; c2 < n ; c2++)
-		dest[c1++] = src[c2];
+	for (c2 = 0 ; c2 < n && src[c2] != '\0' ; c2++)
+		dest[c1 + c2] = src[c2];
+	dest[c1 + c2] = '\0';
 	return (dest);
 }
