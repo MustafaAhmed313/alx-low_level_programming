3e2f18021b4c6700e64b24dcc3b10c8cd3576255
MustafaAhmed313 <mostafa3132004@gmail.com>
2023-03-30 16:37:17 -0700

    fine

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main"
+
+/**
+* _strcat - concate 2 strings
+*
+* @dest: dest array of characters
+* @src: src array of characters
+*
+* Return: pointer of the concatenation string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int c1, c2;
+
+	c1 = 0;
+	while (dest[c1])
+		c1++;
+	for (c2 = 0 ; src[c2] ; c2++)
+		dest[c1++] = src[c2];
+	return (dest);
+}
