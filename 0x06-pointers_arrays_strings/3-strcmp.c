00c2c6b49f105a6338fea9bfaad9fe0dd9756a4e
MustafaAhmed313 <mostafa3132004@gmail.com>
2023-03-30 17:32:58 -0700

    fine

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _strcmp - compare with two strings
+*
+* @s1: string 1
+* @s2: string 2
+*
+* Return: return singed number to compare the strings
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int sum = 0;
+
+	while (*s1)
+	{
+		if (*s1 != *s2)
+		{
+			sum = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (dest);
+}
