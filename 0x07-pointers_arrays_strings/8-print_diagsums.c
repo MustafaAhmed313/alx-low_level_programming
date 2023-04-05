f6319018d711e5f85612bfaed94d1e894aa47124
MustafaAhmed313 <mostafa3132004@gmail.com>
2023-04-04 19:06:48 -0700

    fine

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - print diag sums
+ *
+ * @a: integer
+ * @size: integer
+ *
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int itr, s1 = 0, s2 = 0;
+
+	for (itr = 0; itr < size ; itr++)
+	{
+		s1 += a[itr];
+		s2 += a[size - itr - 1];
+		a += size;
+	}
+	printf("%d, ", s1);
+	printf("%d\n", s2);
+}
