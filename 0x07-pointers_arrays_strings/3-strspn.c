12f62eb1bdb8d6c25d8d45da2b9ba3cba25fd836
MustafaAhmed313 <mostafa3132004@gmail.com>
2023-04-04 18:25:50 -0700

    fine

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of the prefix string
+ *
+ * @s: string
+ * @accept: string
+ *
+ * Return: int iterator
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int itr, jtr;
+
+	for (itr = 0; s[itr] >= '\0'; itr++)
+	{
+		for (jtr = 0; accept[jtr] != s[itr]; jtr++)
+		{
+			if (accept[jtr] == '\0')
+			{
+				return (itr);
+			}
+		}
+	}
+	return (itr);
+}
